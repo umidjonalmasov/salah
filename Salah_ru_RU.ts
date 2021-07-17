3fa82265d53a0042fa4d4c05b6f3416cc11a5ed3
umidjonalmasov <58512900+umidjonalmasov@users.noreply.github.com>
2021-07-17 23:15:36 +0500

    Fix Russian translation

---
+++ Salah_ru_RU.ts
@@ -0,0 +1,246 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>Salah</source>
+        <translation>Salah</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="27"/>
+        <source>Fajr</source>
+        <translation>Фаджр</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="35"/>
+        <source>Begins at dawn, may be performed up to sunrise.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="49"/>
+        <location filename="../mainwindow.ui" line="1133"/>
+        <location filename="../mainwindow.ui" line="1228"/>
+        <location filename="../mainwindow.ui" line="1282"/>
+        <source>2 rakat sunnah</source>
+        <translation>2 ракяата сунны</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="64"/>
+        <source>The Adhan is recited first.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="110"/>
+        <source>Then the Adhan dua is recited.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="150"/>
+        <source>When it is time for the Prayer, perform ablution, wear clean clothes, stand in a clean place, face the qibla and make intentions. For example, I intend to recite the two rakat of the Fajr prayer for the sake of Allah. The intention can be whispered and spoken in tongues as well as inwardly.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="164"/>
+        <source>The prayer begins with raising the palms of both hands towards the qibla, touching the thumb to the earlobe, and saying the takbir &quot;Allahu Akbar&quot;. Women raise their arms to the level of their shoulders.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="221"/>
+        <source>&quot;Sana&quot; (&quot;Subhanakallahumma ...&quot;) is recited with the hands folded under the navel. Women tie their hands and hold them over their breasts.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="261"/>
+        <source>Taawwuz (&quot;Aʼuzu billahi ...&quot;) is recited.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="301"/>
+        <location filename="../mainwindow.ui" line="923"/>
+        <source>Basmala (&quot;Bismillah ...&quot;) is recited.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="341"/>
+        <location filename="../mainwindow.ui" line="963"/>
+        <source>If we are reciting it alone, we are reciting Surat al-Fatiha. Then we say &quot;Amen&quot; inside.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="381"/>
+        <location filename="../mainwindow.ui" line="1003"/>
+        <source>After Surat al-Fatihah, a surah or a longer ayat or at least three short ayats, is recited.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="438"/>
+        <source>Bow down and say, &quot;Allahu Akbar&quot;. In ruku, men stand upright without bending their knees, while women bend their knees slightly. Men lean on their knees with their toes slightly open, while women hold their ankles without opening their toes.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="478"/>
+        <source>Say three times &quot;Subhana Rabbiyal Azim&quot;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="535"/>
+        <source>While restoring the waist say &quot;Sami&apos;Allahu liman hamidah&quot;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="575"/>
+        <source>When you are fully recovered say &quot;Robbana lakal hamd.&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="632"/>
+        <source>Saying &quot;Allahu Akbar&quot; the knees are placed on the ground, then the palms, then the nose and forehead. The elbows should not touch the ground or the hips, and all limbs, especially the toes, should face the qibla as far as possible. Women, on the other hand, prostrate with their elbows on the ground and on their hips, their bellies on their thighs.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="672"/>
+        <location filename="../mainwindow.ui" line="826"/>
+        <source>In sajda say three times  &quot;Subhana Robbiyal ʻala&quot;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="729"/>
+        <source>Say &quot;Allahu Akbar&quot; raise your head from sajda and sit with your left foot covered, right foot straight, your toes facing the qibla, and the palms of both hands at the level of your knees.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="786"/>
+        <source>Say &quot;Allahu Akbar&quot; and do sajda second time.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="883"/>
+        <source>Saying &quot;Allahu Akbar&quot; get up without relying on anything to recite the second rakat.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1066"/>
+        <source>2 rakat fardh</source>
+        <translation>2 ракяата фарда</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1085"/>
+        <source>Zuhr</source>
+        <translation>Зухр</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1093"/>
+        <source>From when the sun has passed the zenith, may be performed up to the time of Asr.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1107"/>
+        <source>4 rakat sunnah</source>
+        <translation>4 ракяата сунны</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1120"/>
+        <location filename="../mainwindow.ui" line="1174"/>
+        <location filename="../mainwindow.ui" line="1269"/>
+        <source>4 rakat fardh</source>
+        <translation>4 ракяата фарда</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1152"/>
+        <source>Asr</source>
+        <translation>Аср</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1160"/>
+        <source>From when the shadow cast by an object is twice its length, may be performed up to the time of Maghrib.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1193"/>
+        <source>Maghrib</source>
+        <translation>Магриб</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1201"/>
+        <source>Begins at sunset, may be performed up to the end of dusk.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1215"/>
+        <source>3 rakat fardh</source>
+        <translation>3 ракяата фарда</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1247"/>
+        <source>Isha</source>
+        <translation>Иша</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1255"/>
+        <source>Begins with the night, may be performed up to dawn.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1295"/>
+        <source>3 rakat witr wajib</source>
+        <translation>3 ракяата витр ваджиба</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1326"/>
+        <source>Hide image</source>
+        <translation>Скрыть изображение</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1336"/>
+        <source>I am a man</source>
+        <translation>Я мужчина</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1343"/>
+        <source>I am a woman</source>
+        <translation>Я женщина</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1417"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1423"/>
+        <source>Edit</source>
+        <translation>Правка</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1432"/>
+        <source>Help</source>
+        <translation>Справка</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1446"/>
+        <source>Quit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1458"/>
+        <source>Previous</source>
+        <translation>Предыдущий</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1467"/>
+        <source>Next</source>
+        <translation>Следующий</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1476"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1485"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+</context>
+</TS>
